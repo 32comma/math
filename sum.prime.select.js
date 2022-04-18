60e3ea04f481f2d4ca001238637d615a24e193bc
rhythmlogger <louis.develog@gmail.com>
2022-04-18 16:32:02 +0900

    update

--- sum.prime.select.js
+++ sum.prime.select.js
@@ -1,6 +1,6 @@
 function isPrime(n) {
     if (isNaN(n) || !isFinite(n) || n % 1 || n < 2) return false;
-    var m = Math.sqrt(n); //returns the square root of the passed value
+    var m = Math.sqrt(n); 
     for (var i = 2; i <= m; i++)
         if (n % i == 0) return false;
     return true;
@@ -11,7 +11,7 @@ function solution(nums) {
     for (var i = 0; i < nums.length; i++) {
         for (var j = i + 1; j < nums.length; j++) {
             for (var k = j + 1; k < nums.length; k++) {
-                var value = [nums[i], nums[j], nums[k]];
+                var value = nums[i]+nums[j]+nums[k];
                 if (isPrime(value)) {
                     answer++;
                 }
