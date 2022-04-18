3bea090314ff7e9e7cf1595f062c60515c166de6
rhythmlogger <louis.develog@gmail.com>
2022-04-18 16:30:03 +0900

    update

--- prime.select.set.js
+++ prime.select.set.js
@@ -5,37 +5,23 @@ function isPrime(n) {
         if (n % i == 0) return false;
     return true;
 }
-function solution(nums) {
 
+function solution(nums) {
     var set = new Set();
-    var nums = [1, 2, 3, 4];
-    for (var i = 0; i < nums.length; i++) {
-        for (var j = i; j < nums.length; j++) {
-            for (var k = j; k < nums.length; k++) {
-                set.add([nums[i], nums[j], nums[k]]);
-            }
-        }
-    }
-    console.log(set);
-    var arr = Array.from(set);
-    var set2 = new Set();
-    for (var i = 0; i < arr.length; i++) {
-        var sum = 0;
-        for (var j = 0; j < arr[i].length; j++) {
-            var d = arr[i][j];
-            sum += d;
-            set2.add(sum);
-        }
-    }
-    var arr2 = Array.from(set2);
-    console.log(arr2);
     var answer = 0;
-    for (var i = 0; i < arr2.length; i++) {
-        if (isPrime(arr2[i])) {
-            answer++;
+    for (var i = 0; i < nums.length; i++) {
+        for (var j = i + 1; j < nums.length; j++) {
+            for (var k = j + 1; k < nums.length; k++) {
+                var value = [nums[i], nums[j], nums[k]];
+                set.add(nums[i] + nums[j] + nums[k]);
+                if (isPrime(nums[i] + nums[j] + nums[k])) {
+                    answer++;
+                }
+            }
         }
     }
     console.log(answer);
     return answer;
+
 }
-solution([1,2,3,4]);
+solution([1, 2, 3, 4]);
